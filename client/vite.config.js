c18a9ed419af48f2af8d729d3c5d1d99482b144c
LittleHero05 <isabellaporraslopez@gmail.com>
2024-12-10 13:29:16 -0500

    retry deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this is set to 'dist'
+    outDir: 'dist', // This will create the dist folder inside the client directory
     manifest: true,
     rollupOptions: {
       input: "./src/main.jsx",
